db560ae9186aea6ffd672084d174fec0c983487c
zaki <zaki-munshi@hotmail.co.uk>
2025-01-14 19:34:44 +0000

    Add functs for operators & call inside of operate()
    Allows each operator to be resued & maintained much easier by calling -
    from one place.

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function() {
+    console.log(add(10, 5));       
+    console.log(subtract(10, 5));   
+    console.log(multiply(10, 5));   
+    console.log(divide(10, 5));     
+}
+
+operate();
