d03412e3940824624ecbbbe511e2d73a9f235977
zaki <zaki-munshi@hotmail.co.uk>
2025-01-18 18:19:44 +0000

    Add the buttons inside of the operate() func

--- script.js
+++ script.js
@@ -5,19 +5,7 @@ const operators = document.querySelectorAll('.operators');
 let num1;
 let num2;
 
-digits.addEventListener('click', function(e) {
-    if(e.target.classList.contains('numbers')) {
-        input.value += e.target.textContent;
-        displayNum = input.value;
-        console.log(displayNum)
-    } 
-})
 
-operators.forEach(function(button) {
-    button.addEventListener('click', function(e) {
-        console.log(e.target.textContent);
-    });
-});
 
 const add = function(a, b) {
     return a + b;
@@ -35,14 +23,27 @@ const divide = function(a, b) {
     return a / b;
 }
 
-const operate = function(operator, num1, num2) {
+const operate = function(operatorButton, num1, num2) {
+
+    digits.addEventListener('click', function(e) {
+        if(e.target.classList.contains('numbers')) {
+            input.value += e.target.textContent;
+            num1 = input.value;
+        } 
+    })
+    
+    operators.forEach(function(operatorButton) {
+        operatorButton.addEventListener('click', function(e) {
+            console.log(e.target.textContent);
+        });
+    });
 
    
-        if (operator === '+') {
+        if (operatorButton === '+') {
             return add(num1, num2)
-        } else if (operator === '-') {
+        } else if (operatorButton === '-') {
             return subtract(num1, num2)
-        } else if (operator === '*') {
+        } else if (operatorButton === '*') {
             return multiply(num1, num2)
         } else {
             return divide(num1, num2)
