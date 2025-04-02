95bf8d8de7a33165936bf8895c805864ca1c1f48
zaki <zaki-munshi@hotmail.co.uk>
2025-04-02 21:09:52 +0100

    Create operate() func that will sign and 2 nums -
    This will then operation funcs on the numbers

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2
 }
+
+function operate (operatorSign, num1, num2) {
+    if (operatorSign = '+') {
+        return add()
+    } else if (operatorSign = '-') {
+        return subtract()
+    } else if (operatorSign = '*') {
+        return multiply()
+    } else {
+        return divide()
+    }
+}
+
+operate()
