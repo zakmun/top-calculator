0b7c6a89f49e39a7ca52f21c88b6273b57d4e14f
zaki <zaki-munshi@hotmail.co.uk>
2025-04-05 18:22:04 +0100

    Update divis func so that num1 == 0 OR num 2 == 0

--- script.js
+++ script.js
@@ -66,12 +66,12 @@ function multiply (num1, num2) {
 }
 
 function divide (num1, num2) {
-    if (num1 || num2 == '0') {
+    if (num1 == '0' || num2 == '0') {
         return 'Can\'t divide by 0 you bum'
+    } else {
+        return num1 / num2
     }
 
-    return num1 / num2
-    
 }
 
 function operate (operatorSign, num1, num2) {
